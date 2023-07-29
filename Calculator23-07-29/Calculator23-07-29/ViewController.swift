ff0bdf50284c9911ddedf53b69438f14406c4f77
副山俊輔 <soeyamashunsuke@MacBook-Pro.local>
2023-07-29 18:24:31 +0900

    [add] ファイルを追加

---
+++ Calculator23-07-29/Calculator23-07-29/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Calculator23-07-29
+//
+//  Created by 副山俊輔 on 2023/07/29.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        view.backgroundColor = .white
+    }
+
+
+}
+
