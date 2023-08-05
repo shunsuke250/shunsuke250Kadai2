a18b459a3b67ac870c1ea547ddd3f5aae57398e1
副山俊輔 <soeyamashunsuke@MacBook-Pro.local>
2023-08-05 20:18:10 +0900

    [modify] 循環参照の解消しエラーを列挙型で管理するよう変更

--- Calculator23-07-29/Calculator23-07-29/ViewController.swift
+++ Calculator23-07-29/Calculator23-07-29/ViewController.swift
@@ -11,6 +11,11 @@ import SwiftUI
 
 class ViewController: UIViewController {
 
+    enum Error: Swift.Error {
+        case invalidStringConversion
+        case operatorNotSelected
+    }
+
     private let firstTextField: UITextField = {
         $0.placeholder = "数値を入力"
         $0.borderStyle = .roundedRect
@@ -46,27 +51,30 @@ class ViewController: UIViewController {
         view.backgroundColor = .white
         setConstrains()
 
-        calculateButton.addAction(.init { _ in
-            let answer = self.calculate()
-            // Validation
-            if answer == 0 {
-                self.answerLabel.text = "エラーが発生しました"
-            } else if answer == 1 {
-                self.answerLabel.text = "演算子を選択してください"
-            } else {
-                self.answerLabel.text = ("\(answer)")
+        calculateButton.addAction(.init { [weak self] _ in
+            guard let strongSelf = self else { return }
+
+            do {
+                let answer = try strongSelf.calculate()
+                strongSelf.answerLabel.text = ("\(answer)")
+            } catch Error.operatorNotSelected {
+                strongSelf.answerLabel.text = "演算子を選択してください"
+            } catch {
+                strongSelf.answerLabel.text = "エラーが発生しました"
             }
         }, for: .touchUpInside)
     }
 
-    func calculate() -> Double {
+    func calculate() throws -> Double {
         guard let num1 = Double(firstTextField.text ?? "0"),
               let num2 = Double(secondTextField.text ?? "0") else {
-            return 0 // Error
+            throw Error.invalidStringConversion
         }
         let operation = operationsSegmentedControl.selectedSegmentIndex
 
         switch operation {
+        case -1:
+            throw Error.operatorNotSelected
         case 0:
             return Double(num1 + num2)
         case 1:
@@ -76,7 +84,7 @@ class ViewController: UIViewController {
         case 3:
             return Double(num1 / num2)
         default:
-            return 1 // Error
+            fatalError("selectedSegmentIndex is invalid.")
         }
     }
 
